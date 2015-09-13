696d9ebff4df0684a9fb04f85eb09c172a9cd873
James Dinsdale <hi@molovo.co.uk>
2015-09-13 20:57:36 +0100

    Declare the env() function properly

--- tests/ObfuscatesIdsTest.php
+++ tests/ObfuscatesIdsTest.php
@@ -9,54 +9,59 @@
  * the model in the database, we are unable to test any
  * methods which use the primary key.
  */
-namespace Molovo\ModelTraits\Test;
-
-use Molovo\ModelTraits\Test\Support\Models\TestModel;
-
-/**
- * The trait uses laravel's env() function in the
- * construct. We set up a dummy for that function
- * which just returns the default, so that we don't
- * need to require the entire laravel framework
- * when this value won't be configured anyway
- */
-function \Molovo\ModelTraits\env($key, $default) {
-    return $default;
+namespace Molovo\ModelTraits {
+    /**
+     * The trait uses laravel's env() function in the
+     * construct. We set up a dummy for that function
+     * which just returns the default, so that we don't
+     * need to require the entire laravel framework
+     * when this value won't be configured anyway.
+     */
+    function env($key, $default)
+    {
+        return $default;
+    }
 }
 
-class ObfuscatesIdsTest extends \PHPUnit_Framework_TestCase
-{
-    protected function setUp()
+namespace Molovo\ModelTraits\Test {
+
+    use Molovo\ModelTraits\Test\Support\Models\TestModel;
+
+    class ObfuscatesIdsTest extends \PHPUnit_Framework_TestCase
     {
-        $this->model     = new TestModel;
-        $this->model->id = 1985404696;
+        protected function setUp()
+        {
+            $this->model     = new TestModel;
+            $this->model->id = 1985404696;
+        }
+
+        /**
+         * A basic functional test example.
+         */
+        public function testAttributeObfuscated()
+        {
+            // Ensure obfuscated ID is returned
+            return $this->assertEquals(1985404696, $this->model->id);
+        }
+
+        public function testKeyNotObfuscated()
+        {
+            return $this->assertEquals(7, $this->model->getKey());
+        }
+
+        public function testHiddenAttributeNotObfuscated()
+        {
+            return $this->assertEquals(7, $this->model->__id);
+        }
+
+        public function testAttributeSet()
+        {
+            $this->model->id = 1978505655;
+            $this->assertEquals(2, $this->model->getKey());
+            $this->assertEquals(2, $this->model->__id);
+
+            return $this->assertEquals(1978505655, $this->model->id);
+        }
     }
 
-    /**
-     * A basic functional test example.
-     */
-    public function testAttributeObfuscated()
-    {
-        // Ensure obfuscated ID is returned
-        return $this->assertEquals(1985404696, $this->model->id);
-    }
-
-    public function testKeyNotObfuscated()
-    {
-        return $this->assertEquals(7, $this->model->getKey());
-    }
-
-    public function testHiddenAttributeNotObfuscated()
-    {
-        return $this->assertEquals(7, $this->model->__id);
-    }
-
-    public function testAttributeSet()
-    {
-        $this->model->id = 1978505655;
-        $this->assertEquals(2, $this->model->getKey());
-        $this->assertEquals(2, $this->model->__id);
-
-        return $this->assertEquals(1978505655, $this->model->id);
-    }
 }
