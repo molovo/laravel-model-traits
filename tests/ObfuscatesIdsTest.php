0b12abbe69060ccd01321f50f8192c4bf234459d
James Dinsdale <hi@molovo.co.uk>
2015-09-13 20:56:14 +0100

    Adds a dummy env() function

--- tests/ObfuscatesIdsTest.php
+++ tests/ObfuscatesIdsTest.php
@@ -13,6 +13,17 @@ namespace Molovo\ModelTraits\Test;
 
 use Molovo\ModelTraits\Test\Support\Models\TestModel;
 
+/**
+ * The trait uses laravel's env() function in the
+ * construct. We set up a dummy for that function
+ * which just returns the default, so that we don't
+ * need to require the entire laravel framework
+ * when this value won't be configured anyway
+ */
+function \Molovo\ModelTraits\env($key, $default) {
+    return $default;
+}
+
 class ObfuscatesIdsTest extends \PHPUnit_Framework_TestCase
 {
     protected function setUp()
